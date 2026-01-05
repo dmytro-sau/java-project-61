20b62751ac899b225aeba76287b9eaf85490dd9d
Dmytro Sau <dmytro.sau@proton.me>
2026-01-05 21:43:36 +0100

    Set up Java Gradle project

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+
+plugins {
+    id("java")
+    id("application") // добавляем, чтобы можно было запускать main()
+    id("com.github.ben-manes.versions") version "0.53.0"
+}
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+application {
+    mainClass.set("hexlet.code.App") // указываем главный класс
+}
+tasks.test {
+    useJUnitPlatform()
+}
