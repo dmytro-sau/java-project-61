9f37a6b631eb280197991a8941f4274de099f2a1
Dmytro Sau <dmytro.sau@proton.me>
2026-01-16 23:27:38 +0100

    Ignore .DS_Store files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("application") // добавляем, чтобы можно было запускать main()
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("checkstyle")
 }
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
@@ -24,4 +25,8 @@ tasks.test {
 
 tasks.getByName<JavaExec>("run") {
     standardInput = System.`in`
+}
+checkstyle {
+    toolVersion = "10.12.0" // или последняя версия
+    configDirectory.set(file("config/checkstyle"))
 }
