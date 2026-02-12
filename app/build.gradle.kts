2cbe17aa383b18820be48a013494677f112b8462
Dmytro Sau <dmytro.sau@proton.me>
2026-02-12 16:57:04 +0100

    Add SonarQube configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("application") // добавляем, чтобы можно было запускать main()
     id("com.github.ben-manes.versions") version "0.53.0"
+    id("org.sonarqube") version "7.1.0.6387"
     id("checkstyle")
 }
 group = "hexlet.code"
@@ -29,4 +30,10 @@ tasks.getByName<JavaExec>("run") {
 checkstyle {
     toolVersion = "10.12.0" // или последняя версия
     configDirectory.set(file("config/checkstyle"))
+}
+sonar {
+  properties {
+    property("sonar.projectKey", "dmytro-sau_java-project-61")
+    property("sonar.organization", "dmytro-sau")
+  }
 }
