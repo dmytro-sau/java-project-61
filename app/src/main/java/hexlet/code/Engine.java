cddb93c8d4d912b842c1ca5a3f245c6b439e5eca
Dmytro Sau <dmytro.sau@proton.me>
2026-02-15 21:29:40 +0100

    Fix name prompt output formatting to match expected tests

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,7 +8,7 @@ public class Engine {
     public static void run(String description, String[][] rounds) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
+        System.out.println("May I have your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
         System.out.println(description);
